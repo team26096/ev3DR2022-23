5edf1b87bc2337b86bf6f12509d6ba7d22017d19
CozyBunny <93013834+CozyBunny@users.noreply.github.com>
2022-10-30 15:18:28 -0400

    replaced line following to solar farm to gyro straight (#9)

--- Run3.py
+++ Run3.py
@@ -52,14 +52,17 @@ robot.cs = left_light
 robot.follow_gyro_angle(3, 0, 0, SpeedPercent(25), target_angle=0, 
                     follow_for=follow_until_black, lightSensor=left_light)
 
-#starts line following with left sensor
-robot.cs = left_light
-robot.follow_line(-1.5,0,0,15, target_light_intensity=56,
-                follow_left_edge=True,
-                off_line_count_max=500,
-                sleep_time=0.01,
-                follow_for=my_follow_for_degrees, degrees=900,
-                left_motor = left_motor, right_motor = right_motor)
+# #starts line following with left sensor
+# robot.cs = left_light
+# robot.follow_line(-1.5,0,0,15, target_light_intensity=56,
+#                 follow_left_edge=True,
+#                 off_line_count_max=500,
+#                 sleep_time=0.01,
+#                 follow_for=my_follow_for_degrees, degrees=900,
+#                 left_motor = left_motor, right_motor = right_motor)
+
+gyro.reset()
+pivot_gyro_turn(10, 0, 35, robot, gyro, bLeftTurn=False)
 
 s.beep()
 
@@ -68,7 +71,7 @@ gyro.reset()
 right_motor.reset()
 left_motor.reset()
 robot.follow_gyro_angle(3, 0, 0, SpeedPercent(25), target_angle=0, 
-                    follow_for=my_follow_for_degrees, degrees=600,
+                    follow_for=my_follow_for_degrees, degrees=675,
                     right_motor = right_motor, left_motor = left_motor)
 
 #turn to collect energy units from solar farm
@@ -80,24 +83,26 @@ gyro.reset()
 right_motor.reset()
 left_motor.reset()
 robot.follow_gyro_angle(3, 0, 0, SpeedPercent(25), target_angle=0, 
-                    follow_for=my_follow_for_degrees, degrees=100,
+                    follow_for=my_follow_for_degrees, degrees=165,
                     right_motor = right_motor, left_motor = left_motor)
 
 #turn to collect energy units from solar farm
 gyro.reset()
-pivot_gyro_turn(0, -20, 30, robot, gyro, bLeftTurn=False)
+pivot_gyro_turn(0, -20, 35, robot, gyro, bLeftTurn=False)
 
 #gyro straight to collect the last solar unit (next to smart grid)
 gyro.reset()
 right_motor.reset()
 left_motor.reset()
 robot.follow_gyro_angle(3, 0, 0, SpeedPercent(25), target_angle=0, 
-                    follow_for=my_follow_for_degrees, degrees=320,
+                    follow_for=my_follow_for_degrees, degrees=380,
                     right_motor = right_motor, left_motor = left_motor)
 
 #turn to drop energy units into power to X
 gyro.reset()
 pivot_gyro_turn(0, -20, 70, robot, gyro, bLeftTurn=False)
+#raising rack to avoid water reservoir
+mm_vertical.on_for_degrees(100, 500, brake=True, block=False)
 
 #gyro straight into power to X to drop energy units
 gyro.reset()
