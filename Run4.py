88de2c2ba7fe9fc19997fb83d854b7111fd5ceda
Anusha15agg <85901498+Anusha15agg@users.noreply.github.com>
2022-11-02 18:12:45 -0400

    Completed 3 missions, aligned to wind turbine straight ahead, using front of rack (#11)

--- Run4.py
+++ Run4.py
@@ -17,6 +17,8 @@ from ev3dev2._platform.fake import OUTPUT_C
 from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveDifferential, SpeedRPM
 from ev3dev2.wheel import EV3Tire, Wheel
 from functions import *
+from functions import follow_until_black
+from functions import follow_until_white
 
 s = Sound()
 robot = MoveTank(OUTPUT_B, OUTPUT_C)
@@ -42,52 +44,71 @@ logfile.info('gyro before = ' + str(gyro.angle))
 left_motor.reset()
 right_motor.reset()
 run_for_motor_stalled(mm_horizontal, 10000, -35)
+
 #we reset mm_horizontal
 mm_horizontal.reset()
+
 #we run mm_vertical all the way down
 run_for_motor_stalled(mm_vertical, 10000, -50)
+
 # gyro straight to align with rechargeable battery 
 mdiff.follow_gyro_angle(4, 0, 0, 30, target_angle=0, 
                             follow_for=my_follow_for_degrees, degrees=615,
                             right_motor = right_motor, left_motor = left_motor)
+
 # move horizontal rack left to drop battery units in rechargeable battery space.
 run_for_motor_stalled(mm_horizontal, 10000, 35)
+
 # bring rack back to position and right for watch tv
 mm_horizontal.reset()
 run_for_motor_stalled(mm_horizontal, 10000, -35)
+
 # gyro staight to complete watch television
-#gyro.reset()
 left_motor.reset()
 right_motor.reset()
 mdiff.follow_gyro_angle(4, 0, 0, 30, target_angle=0, 
-                            follow_for=my_follow_for_degrees, degrees=220,
+                            follow_for=my_follow_for_degrees, degrees=280,
                             right_motor = right_motor, left_motor = left_motor)
+
 # move horizontal rack to the left to do wind turbine mission
 run_for_motor_stalled(mm_horizontal, 10000, 35)
+
 # lifting rack up to avoid hitting wind turbine
 mm_vertical.on_for_degrees(100, 1500, brake=True, block=False)
+
 # move forward to align with white line in wind turbine
 left_motor.reset()
 right_motor.reset()
 mdiff.follow_gyro_angle(4, 0, 0, 20, target_angle=0, 
                             follow_for=follow_until_white, lightSensor = right_light)
+s.beep()
 mdiff.follow_gyro_angle(4, 0, 0, 20, target_angle=0, 
                             follow_for=follow_until_black, lightSensor = right_light)
+s.beep()
+
 # turn left to align with oil platform 
-pivot_gyro_turn(0, 20, -5, robot, gyro, bLeftTurn=True)
+pivot_gyro_turn(0, 20, -15, robot, gyro, bLeftTurn=True)
+
 # gyro staight to complete oil platform
 gyro.reset()
 left_motor.reset()
 right_motor.reset()
 mdiff.follow_gyro_angle(4, 0, 0, 20, target_angle=0, 
-                            follow_for=my_follow_for_degrees, degrees=180,
+                            follow_for=my_follow_for_degrees, degrees=200,
                             right_motor = right_motor, left_motor = left_motor)
+
 # gyro staight backwards to realse the truck
 gyro.reset()
 left_motor.reset()
 right_motor.reset()
 mdiff.follow_gyro_angle(4, 0, 0, -20, target_angle=0, 
                             follow_for=follow_until_white, lightSensor = right_light)
+
+# turn to align front with wind turbine
+pivot_gyro_turn(20, -20, 60, robot, gyro, bLeftTurn=False)
+
+# commenting v1 plan for wind turbine bucket thing
+'''
 # turn left to align with oil platform 
 pivot_gyro_turn(0, 20, -10, robot, gyro, bLeftTurn=True)
 gyro.reset()
@@ -96,11 +117,22 @@ right_motor.reset()
 mdiff.follow_gyro_angle(4, 0, 0, -20, target_angle=0, 
                             follow_for=follow_until_black, lightSensor = right_light)
 mdiff.follow_gyro_angle(4, 0, 0, -20, target_angle=0, 
-                            follow_for=follow_until_white, lightSensor = right_light) 
+                            follow_for=follow_until_white, lightSensor = right_light)
+        
 #we run mm_vertical all the way down
 run_for_motor_stalled(mm_vertical, 10000, -50)
 gyro.reset()
 left_motor.reset()
 right_motor.reset()
 robot.on_for_degrees(-25, -25, 80, brake=True, block=True)
-run_for_motor_stalled(mm_horizontal, 10000, -35)
+
+# the robot will push the lever 4 times to get the energy units
+loop = 0
+while(True):
+    if (loop < 4):
+        run_for_motor_stalled(mm_horizontal, 10000, -50)
+        sleep(2)
+        run_for_motor_stalled(mm_horizontal, 10000, 50) 
+        sleep(2)
+    loop = loop + 1
+'''
