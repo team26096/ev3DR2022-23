f1d0ed90073f7b77930bbc1bad14eab6f3534006
SaanviNan <114314629+SaanviNan@users.noreply.github.com>
2023-03-13 20:20:25 -0400

    We finished run 2 pumping and retreiving fuel truck!!!!! All that is left is speeding up some parts (#69)

--- testColorSensors.py
+++ testColorSensors.py
@@ -2,15 +2,21 @@
 
 import ev3dev2.sensor as sensor
 import ev3dev2.sensor.lego as lego_sensor
+from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4
 
 # Initialize the color sensor object
-color_sensor = lego_sensor.ColorSensor()
+left_light = lego_sensor.ColorSensor(INPUT_4)
+right_light = lego_sensor.ColorSensor(INPUT_1)
+back_light = lego_sensor.ColorSensor(INPUT_2)
 
 # Set the color sensor mode to "raw reflected light"
-color_sensor.mode = sensor.MODE_REFLECT_RAW
+#left_light.mode = left_light.MODE_REF_RAW
+left_light._mode('REF-RAW')
 
-# Read the raw sensor value
-raw_value = color_sensor.value(0)
+# right_light.mode = right_light.MODE_REF_RAW
+# back_light.mode = back_light.MODE_REF_RAW
 
 # I think value(0) corresponds to red color, green is 1 and blue is 2
-print("Raw value:", raw_value)
+print("left Raw value:", left_light._value(0))
+# print("right Raw value:", right_light.value(0))
+# print("back Raw value:", back_light.value(0))
